c7ed59b707e5d0393b32f1e5a41b2e140743ac35
andikastp <andikapangestu79@gmailcom>
2023-06-14 15:49:44 +0700

    Added Header components

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex-col w-full flex-center">
+      <h1 className="head_text text_center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="text-center orange_gradient">AI-Powered Prompts</span>
+        <p className="text-center desc">
+          Promptopia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts
+        </p>
+      </h1>
+
+      //feeds
+    </section>
+  );
 };
 
 export default Home;
